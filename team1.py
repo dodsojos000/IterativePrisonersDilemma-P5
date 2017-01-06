75d19928e367efc4d907dd8f2fe541816b146341
leifada000 <leifada000@mysbisd.org>
2017-01-06 12:29:59 -0600

    Revert "Just Testing"
    This reverts commit 2ee6572240ddaa14e391df52844424ed439ba122.

--- team1.py
+++ team1.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'NyQuil' # Only 10 chars displayed.
-strategy_name = 'DayQuil'
+strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
