4fae7ec6dae456aefc78ddbd699f389545504bdd
dragon310798 <nguyehoa020@mysbisd.org>
2017-01-06 12:09:56 -0600

    Name changing
    I changed the name of the program

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Demacia' # Only 10 chars displayed.
+strategy_name = 'Apolo700'
+strategy_description = 'I do not know'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
